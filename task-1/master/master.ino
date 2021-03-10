6f9dca2f6f5113f7b974df72279c7bbf688482eb
Jacob Koziej <jacobkoziej@gmail.com>
2021-03-10 09:41:03 -0500

    Add comment to clear up string length

--- task-1/master/master.ino
+++ task-1/master/master.ino
@@ -23,7 +23,7 @@
 uint8_t even_nums[6] = {0, 2, 4, 6, 8, 10};
 uint8_t odd_nums[6]  = {1, 3, 5, 7, 9, 11};
 uint8_t some_nums[6] = {0, 1, 2, 3, 4,  5};
-const char *unfunny_message = "joe mama!\?";
+const char *unfunny_message = "joe mama!\?";  // 10 char + NUL-terminator
 
 
 void setup()
