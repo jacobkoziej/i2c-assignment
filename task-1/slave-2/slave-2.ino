6396da9503b973ba913b861574b1c7ac7e8ac52c
Jacob Koziej <jacobkoziej@gmail.com>
2021-03-12 11:18:28 -0500

    Add task-1 slave-2

---
+++ task-1/slave-2/slave-2.ino
@@ -0,0 +1,52 @@
+/*
+ * Task 1 (slave-2)
+ * Copyright (C) 2021  Jacob Koziej <jacobkoziej@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <Wire.h>
+
+#include "common.h"
+
+uint8_t msg_buffer[6];
+void *i2c_data;
+size_t i2c_datasiz;
+
+
+void setup()
+{
+	i2c_data    = msg_buffer;
+	i2c_datasiz = sizeof(msg_buffer);
+
+	Serial.begin(9600);
+
+	Wire.begin(I2C_SLAVE_2);
+	Wire.onReceive(i2c_receive_handler);
+}
+
+void loop()
+{
+	Serial.print("Bytes in:");
+	for (uint8_t i = 0; i < sizeof(msg_buffer); i++) {
+		Serial.print(" ");
+		Serial.print(msg_buffer[i], HEX);
+	}
+	Serial.println();
+}
+
+void i2c_receive_handler(int bytes)
+{
+	receive_data(i2c_data, i2c_datasiz, bytes);
+}
