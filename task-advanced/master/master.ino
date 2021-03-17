dd19b3e6dcfe80e8826c4c5ab061068a4e2b06cf
Jacob Koziej <jacobkoziej@gmail.com>
2021-03-17 16:13:26 -0400

    Correct typo in "Advanced"

--- task-advanced/master/master.ino
+++ task-advanced/master/master.ino
@@ -1,5 +1,5 @@
 /*
- * Task Advnaced (master)
+ * Task Advanced (master)
  * Copyright (C) 2021  Jacob Koziej <jacobkoziej@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
