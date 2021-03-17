ad0444c7b1d75378ba780c126a5067725f3c8a7d
Jacob Koziej <jacobkoziej@gmail.com>
2021-03-17 16:56:55 -0400

    Add task-advanced slave-3

---
+++ task-advanced/slave-3/slave-3.ino
@@ -0,0 +1,44 @@
+/*
+ * Task Advanced (slave-3)
+ * Copyright (C) 2021  Jacob Koziej <jacobkoziej@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <Wire.h>
+
+#include "common.h"
+
+uint8_t color[3];
+void *i2c_data;
+size_t i2c_datasiz;
+
+
+void setup()
+{
+	i2c_data    = color;
+	i2c_datasiz = sizeof(color);
+
+	Wire.begin(I2C_SLAVE_3);
+	Wire.onReceive(i2c_receive_handler);
+}
+
+void loop()
+{
+}
+
+void i2c_receive_handler(int bytes)
+{
+	receive_data(i2c_data, i2c_datasiz, bytes);
+}
